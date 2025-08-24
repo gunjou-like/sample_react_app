f2ab288bf77e43f83edc42a711deb4c7bec846c9
gunjou-like <koyabu721na@gmail.com>
2025-08-24 11:51:38 +0900

    Datalist定義

---
+++ sample/src/components/Datalist.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { Data } from "../types/Data";
+import { fetchData } from "../services/Dataservice";
+
+const DataList: React.FC = () => {
+    const [data, setData] = useState<Data[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchData(3)
+            .then((data) => {
+            setData(data);
+            setLoading(false);
+            })
+            .catch((err) => {
+            console.error(err);
+            setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <p>読み込み中...</p>;
+
+    return (
+        <div>
+            <h2>選手一覧（APIから取得）</h2>
+        <ul>
+        {data.map((d) => (
+            <li key={d.id}>
+                {d.name} - 定価:{d.price} 在庫:{d.stock}
+            </li>
+        ))}
+        </ul>
+    </div>
+    );
+};
+
+export default DataList;
